ed805d06ddd7ae0bf34dbfd2248c353a59a45929
riffling <emerson_gong@hotmail.com>
2018-12-20 22:45:59 +0800

    'init'

---
+++ Hello.lua
@@ -0,0 +1,35 @@
+#!/usr/local/bin/lua
+--comments
+--[[
+  multiple line comments
+]]
+a=23;
+b=a+23;
+print(b);
+print(a++);
+
+
+print(type("Hello world"))      --> string
+print(type(10.4*3))             --> number
+print(type(print))              --> function
+print(type(type))               --> function
+print(type(true))               --> boolean
+print(type(nil))                --> nil
+print(type(type(X)))            --> string
+
+--对于全局变量和 table，nil 还有一个"删除"作用，给全局变量或者 table 表里的变量赋一个 nil 值，等同于把它们删掉，执行下面代码就知：
+
+tab1 = { key1 = "val1", key2 = "val2", "val3" }
+for k, v in pairs(tab1) do
+    print(k .. " - " .. v)
+end
+ 
+tab1.key1 = nil
+for k, v in pairs(tab1) do
+    print(k .. " - " .. v)
+end
+
+type(X)  --nil
+type(X)==nil  --false
+ type(X)=="nil"  --true
+
